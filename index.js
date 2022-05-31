9b1dd88b8b3aabb073df71942e4f03499f0fb07c
Lorena Capotorto <lorycap2000@gmail.com>
2022-05-31 18:54:20 +0200

    Modifica file index e Procfile

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.use(express.static(path.join(__dirname, './public')));
+
+
+app.get('/', function(req, res) {
+  res.send('Hello World!')
+});
+
+app.listen(PORT, function(){
+  console.log('Server in ascolto sulla porta: ' + PORT);
+});
